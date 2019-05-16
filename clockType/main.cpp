85402ff44d63b577a67b8a100795eba67cba7f94
Isaac Delgado <isaacdelgado@isaacs-macbook-pro.local>
2019-05-15 19:26:16 -0700

    Test #2

--- clockType/main.cpp
+++ clockType/main.cpp
@@ -10,6 +10,6 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World this is test to push to git hub from xcode!\n";
     return 0;
 }
